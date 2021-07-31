bac5d654f089776bb920dc6154566dd5004a27b1
TakiKauser <takikauserbadhon@gmail.com>
2021-08-01 02:47:15 +0600

    conditionals

---
+++ conditional.js
@@ -0,0 +1,25 @@
+var isFoodReady = false;
+
+if(isFoodReady == true){
+    console.log("Go to eat.");
+}
+
+var pcPrice = 89000;
+var budget = 160000;
+
+if(pcPrice > budget){
+    console.log("Gonna buy it next time.");
+}
+if(pcPrice < budget){
+    console.log("I got my dream pc!");
+}
+
+var bikePrice = 53000;
+var bikeBudget = 50000;
+
+if(bikePrice < bikeBudget){
+    console.log("my beast");
+}
+else{
+    console.log("gonna buy a cycle");
+}

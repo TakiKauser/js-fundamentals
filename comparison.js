fd3557c484fd2c8d97329db8d9eabbbfd7dfc871
TakiKauser <takikauserbadhon@gmail.com>
2021-08-01 02:28:15 +0600

    comparisons

---
+++ comparison.js
@@ -0,0 +1,8 @@
+console.log(3 > 9);
+console.log(3 < 9);
+console.log(3 == 9);
+console.log(9 == 9);
+console.log(3 != 3);
+
+console.log(studyGood && goodPerson);
+console.log(isBCS || goodResult);

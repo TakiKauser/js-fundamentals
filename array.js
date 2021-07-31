916cef6e4635bf55666717a33861bf283874a936
TakiKauser <takikauserbadhon@gmail.com>
2021-08-01 01:31:40 +0600

    array length, index

---
+++ array.js
@@ -0,0 +1,23 @@
+var friendsAge = [13, 17, 15, 29];
+
+var heroine = ["shostika", "sai pallovi", "poriniti"];
+
+// array.length
+console.log(heroine.length);
+
+// array.indexof(element);
+
+itemIndex = heroine.indexOf("sai pallovi");
+console.log(itemIndex);
+itemIndex2 = heroine.indexOf("dhanus");
+console.log(itemIndex2);
+
+// index[]
+console.log(friendsAge[3]);
+console.log(heroine[1]);
+
+console.log(friendsAge);
+friendsAge[5] = 23;
+console.log(friendsAge);
+friendsAge[4] = 0;
+console.log(friendsAge);
